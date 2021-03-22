87a8dc647444e1af0a79235cb93cd31b72055b99
xiaotongqiu <qcatsolution@gmail.com>
2021-03-22 23:38:37 +0800

    第 k 个最小元素

---
+++ leetcode/editor/cn/[230]二叉搜索树中第K小的元素.java
@@ -0,0 +1,79 @@
+//给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,1,4,null,2], k = 1
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,3,6,2,4,null,null,1], k = 3
+//输出：3
+// 
+//
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数为 n 。 
+// 1 <= k <= n <= 104 
+// 0 <= Node.val <= 104 
+// 
+//
+// 
+//
+// 进阶：如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化算法？ 
+// Related Topics 树 二分查找 
+// 👍 368 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        // 升序遍历bst -> 中序遍历
+        tranverse(root, k);
+        return res;
+    }
+    int rank = 0;
+    int res = 0;
+    public void tranverse(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        tranverse(root.left, k);
+        rank ++;
+
+        if (rank == k) {
+            res = root.val;
+            return;
+        }
+
+        tranverse(root.right, k);
+
+        return;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
